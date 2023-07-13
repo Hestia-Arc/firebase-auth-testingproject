2735108cc7614b4ccc8cf77a4fe5eae0cf1c37f0
Afolabi Esther <starafolab@gmail.com>
2023-07-13 18:18:06 +0100

    Update utils

--- src/firebase/firebaseUtil.js
+++ src/firebase/firebaseUtil.js
@@ -1,7 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth'
-import {collection, doc, getDoc, getFirestore, setDoc} from 'firebase/firestore'
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
+import {
+  collection,
+  doc,
+  getDoc,
+  getFirestore,
+  setDoc,
+} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,7 +24,7 @@ const firebaseConfig = {
   projectId: "login-testing-8c816",
   storageBucket: "login-testing-8c816.appspot.com",
   messagingSenderId: "508516212462",
-  appId: "1:508516212462:web:952dfb2c99198da0685dff"
+  appId: "1:508516212462:web:952dfb2c99198da0685dff",
 };
 
 // Initialize Firebase
@@ -20,53 +32,66 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth();
 
+// ------------------------------
+// GOOGLE PROVIDER
+// -------------------------------
 const provider = new GoogleAuthProvider();
-const db = getFirestore(app);
 
 export const signInWithGoogle = async () => {
-    const user = await signInWithPopup(auth, provider)
-    return user;
-} 
+  const user = await signInWithPopup(auth, provider);
+  return user;
+};
 
+// ------------------------
+// DATABASE
+// --------------------------
+const db = getFirestore(app);
+
+// --------------------------
+// LOGIN WITH EMAIL&PASSWORD
+// -----------------------------
 export const loginWithEmailAndPassword = async (email, password) => {
- const user = await signInWithEmailAndPassword(auth, email, password)
+  const user = await signInWithEmailAndPassword(auth, email, password);
 
- return user;
-}
+  return user;
+};
 
+// --------------------------
+// SIGNUP WITH EMAIL&PASSWORD
+// --------------------------
 export const signUpWithEmailAndPassword = async (email, password) => {
   const user = await createUserWithEmailAndPassword(auth, email, password);
 
   return user;
-}
+};
 
+// ----------------------------------
+// CREATE NEW USER IN DATABASE
+// -------------------------------
 export const createUserProfile = async (userAuth, additionalData) => {
-    
-    if (!userAuth) {
-      return;
+  if (!userAuth) {
+    return;
+  }
+
+  const usersRef = collection(db, "users");
+  const docRef = doc(db, "users", userAuth.uid);
+  const user = await getDoc(docRef);
+
+  if (!user.exists()) {
+    const { displayName, email } = userAuth;
+    const createAt = new Date();
+
+    try {
+      await setDoc(doc(usersRef, userAuth.uid), {
+        displayName,
+        email,
+        createAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user" + error.message);
     }
+  }
 
-    const usersRef = collection(db, 'users');
-    const docRef = doc(db, 'users', userAuth.uid);
-    const user = await getDoc(docRef);
-
-    if (!user.exists()) {
-      const {displayName, email} = userAuth;
-      const createAt = new Date();
-
-
-      try {
-        await setDoc(doc(usersRef, userAuth.uid), {displayName, email, createAt, ...additionalData});
-      } catch(error) {
-        console.log('error creating user' + error.message);
-      }
-
-
-    } 
-
-    return docRef;
-
-  
-}
-
-
+  return docRef;
+};
